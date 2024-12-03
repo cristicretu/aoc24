94402a49a4775a61b00fe00f42f410ca1c4f2270
Cristian Cretu <crisemcr@gmail.com>
2024-12-03 07:13:49 +0200

    feat: cleanup

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -15,27 +15,47 @@ fn reader(input_file: String, corr_str: &mut String) {
     }
 }
 
-fn main() {
-    let mut corr_str = String::new();
-    reader("1.txt".to_string(), &mut corr_str);
-    println!("{}", corr_str);
-
-    let re = Regex::new(r"(mul\(([\d,]+)\)|don\'t\(\)|do\(\))").unwrap();
+fn solve1(corr_str: &str) -> i32 {
+    let re = Regex::new(r"mul\(([\d,]+)\)").unwrap();
     let matches = re.find_iter(&corr_str);
-    let mut ans1 = 0;
-    let mut ok = true;
+    let mut sum = 0;
     for mat in matches {
-        println!("{}", mat.as_str());
+        let ln = mat.as_str();
+        let ln = &ln[4..ln.len() - 1];
+        let numbers: Vec<i32> = ln.split(",").map(|s| s.parse::<i32>().unwrap()).collect();
+        sum += numbers[0] * numbers[1];
+    }
+    sum
+}
+
+fn solve2(corr_str: &str) -> i32 {
+    let re: Regex = Regex::new(r"(mul\(([\d,]+)\)|don\'t\(\)|do\(\))").unwrap();
+    let matches = re.find_iter(&corr_str);
+    let mut ok = true;
+
+    let mut sum = 0;
+
+    for mat in matches {
         let ln = mat.as_str();
         if ln.contains("mul") && ok {
             let ln = &ln[4..ln.len() - 1];
             let numbers: Vec<i32> = ln.split(",").map(|s| s.parse::<i32>().unwrap()).collect();
-            ans1 += numbers[0] * numbers[1];
+            sum += numbers[0] * numbers[1];
         } else if ln.contains("don't") {
             ok = false;
         } else if ln.contains("do") {
             ok = true;
         }
     }
-    println!("{}", ans1);
+    sum
+}
+
+fn main() {
+    let mut corr_str = String::new();
+    reader("1.txt".to_string(), &mut corr_str);
+
+    let ans1 = solve1(&corr_str);
+    let ans2 = solve2(&corr_str);
+    println!("Part 1: {}", ans1);
+    println!("Part 2: {}", ans2);
 }

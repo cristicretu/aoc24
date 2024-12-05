cf123e8e92630e880919c124ed9ad89f206f4bd0
Cristian Cretu <crisemcr@gmail.com>
2024-12-05 09:58:12 +0200

    damn

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,6 +1,7 @@
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     fs::File,
+    hash::Hash,
     io::{BufRead, BufReader},
 };
 
@@ -26,10 +27,96 @@ fn reader(input_file: String, dict: &mut HashMap<i32, Vec<i32>>, updates: &mut V
     }
 }
 
+fn process(dict: &mut HashMap<i32, Vec<i32>>) -> Vec<i32> {
+    let mut all_numbers = HashSet::new();
+    for (&key, values) in dict.iter() {
+        all_numbers.insert(key);
+        all_numbers.extend(values);
+    }
+
+    // Find the "root" nodes - numbers that don't appear as values in any rule
+    let mut root_nodes: Vec<i32> = all_numbers
+        .iter()
+        .filter(|&&num| !dict.values().any(|values| values.contains(&num)))
+        .cloned()
+        .collect();
+
+    root_nodes.sort_by(|a, b| b.cmp(a));
+
+    // Start with all root nodes
+    let mut order = root_nodes;
+
+    let mut changed = true;
+    while changed {
+        changed = false;
+        for i in 0..order.len() {
+            let element = order[i];
+            if let Some(values) = dict.remove(&element) {
+                order.splice(i + 1..i + 1, values.iter().cloned());
+                changed = true;
+
+                let mut seen = HashSet::new();
+                let mut remove_indices = Vec::new();
+                for j in (0..order.len()).rev() {
+                    if !seen.insert(order[j]) {
+                        remove_indices.push(j);
+                    }
+                }
+                for idx in remove_indices.into_iter() {
+                    order.remove(idx);
+                }
+                break;
+            }
+        }
+    }
+
+    order
+}
+
+fn solve1(correct_order: Vec<i32>, updates: Vec<Vec<i32>>) -> i32 {
+    let mut sum_middle_pages = 0;
+
+    for update in updates {
+        let mut is_valid = true;
+        let mut last_pos = -1;
+
+        for &num in &update {
+            if let Some(pos) = correct_order.iter().position(|&x| x == num) {
+                if last_pos > pos as i32 {
+                    is_valid = false;
+                    break;
+                }
+                last_pos = pos as i32;
+            }
+        }
+
+        if is_valid {
+            let middle_idx = update.len() / 2;
+            let middle_value = update[middle_idx];
+            sum_middle_pages += middle_value;
+
+            println!("Valid update: {:?}, middle value: {}", update, middle_value);
+        } else {
+            println!("Invalid update: {:?}", update);
+        }
+    }
+
+    sum_middle_pages
+}
+
 fn main() {
-    let mut dict: HashMap<i32, Vec<i32>> = HashMap::new();
+    let mut dict = HashMap::new();
     let mut updates: Vec<Vec<i32>> = Vec::new();
     reader("example.txt".to_string(), &mut dict, &mut updates);
     println!("{:?}", dict);
-    println!("{:?}", updates);
+
+    let mut sorted_dict: Vec<(i32, Vec<i32>)> = dict.into_iter().collect();
+    sorted_dict.sort_by_key(|(key, _)| -key);
+    let mut dict = sorted_dict.into_iter().collect();
+
+    let correct_order = process(&mut dict);
+    println!("{:?}", correct_order);
+
+    let ans1 = solve1(correct_order, updates);
+    println!("{}", ans1);
 }
